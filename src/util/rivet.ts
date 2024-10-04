bf10f38058fcad08bda392b20ddf1e9f3e3e61ef
Jack Vatcharat <vrattananun@palo-it.com>
2024-10-04 11:54:45 +0700

    remove unused function

--- src/util/rivet.ts
+++ src/util/rivet.ts
@@ -1,23 +1,10 @@
 import { LooseDataValue } from '@ironclad/rivet-node'
-import { GraphResultValue } from '../types/rivet'
 
 export function createRivetInputs(input: string): Record<string, LooseDataValue> {
   return {
     input: {
       type: 'string',
       value: input,
-    },
-  }
-}
-export function createLoopRivetInputs(input: string, solutions: GraphResultValue[]): Record<string, LooseDataValue> {
-  return {
-    input: {
-      type: 'string',
-      value: input,
-    },
-    previousSolutions: {
-      type: 'string',
-      value: solutions.map((e) => e.solution).join('\n'),
     },
   }
 }
